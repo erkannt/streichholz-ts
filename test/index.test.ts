802f4437becb32f7cd23643bd3d4ad95c3df524c
Daniel Haarhoff <daniel@rknt.de>
2023-02-13 18:32:25 +0000

    cover default case with test

--- test/index.test.ts
+++ test/index.test.ts
@@ -22,4 +22,25 @@ describe("cond", () => {
     expect(behaviour2).toHaveBeenCalledWith(input);
     expect(defaultBehaviour).not.toHaveBeenCalled;
   });
+
+  it("passes input to default behaviour if none of the conditions hold true", () => {
+    const behaviour1 = jest.fn(() => undefined);
+    const behaviour2 = jest.fn(() => undefined);
+    const defaultBehaviour = jest.fn(() => undefined);
+    const input = 5;
+
+    pipe(
+      input,
+      cond(
+        [
+          [(n) => false, behaviour1],
+          [(n) => false, behaviour2],
+        ],
+        defaultBehaviour
+      )
+    );
+    expect(behaviour1).not.toHaveBeenCalled;
+    expect(behaviour2).not.toHaveBeenCalled;
+    expect(defaultBehaviour).toHaveBeenCalledWith(input);
+  });
 });
