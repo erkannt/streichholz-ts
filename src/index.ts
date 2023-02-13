8a25b7af31515fcd7518eb2041fec3f6c5c5f410
Daniel Haarhoff <daniel@rknt.de>
2023-02-13 18:18:01 +0000

    Start shaping a test for cond

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const hello = "Well hello there.";
+import { Lazy } from "fp-ts/lib/function";
+import { Predicate } from "fp-ts/lib/Predicate";
+
+export const cond =
+  <A>(
+    cases: ReadonlyArray<[Predicate<A>, (a: A) => A]>,
+    defaultBehaviour: (a: A) => A
+  ) =>
+  (input: A): A => {
+    return input;
+  };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,25 @@
-import { hello } from "../src";
+import { pipe } from "fp-ts/function";
+import { cond } from "../src";
 
-describe("hello world", () => {
-  it("contains hello", () => {
-    expect(hello).toContain("hello");
+describe("cond", () => {
+  it.failing("passes input to first condition that returns true", () => {
+    const behaviour1 = jest.fn(() => 1);
+    const behaviour2 = jest.fn(() => 1);
+    const defaultBehaviour = jest.fn(() => 1);
+    const input = 5;
+
+    pipe(
+      input,
+      cond<number>(
+        [
+          [(n) => n != input, behaviour1],
+          [(n) => n == input, behaviour2],
+        ],
+        defaultBehaviour
+      )
+    );
+    expect(behaviour1).not.toHaveBeenCalled;
+    expect(behaviour2).toHaveBeenCalledWith(input);
+    expect(defaultBehaviour).not.toHaveBeenCalled;
   });
 });
