b1da7af551e6f5896a198aca3b450c7de2f7dad6
Daniel Haarhoff <daniel@rknt.de>
2023-02-13 18:19:59 +0000

    Extract a type to make working on it easier

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Lazy } from "fp-ts/lib/function";
 import { Predicate } from "fp-ts/lib/Predicate";
 
-export const cond =
-  <A>(
-    cases: ReadonlyArray<[Predicate<A>, (a: A) => A]>,
-    defaultBehaviour: (a: A) => A
-  ) =>
-  (input: A): A => {
-    return input;
-  };
+type Cond = <A>(
+  cases: ReadonlyArray<[Predicate<A>, (a: A) => A]>,
+  defaultBehaviour: (a: A) => A
+) => (input: A) => A;
+
+export const cond: Cond = () => (input) => {
+  return input;
+};
