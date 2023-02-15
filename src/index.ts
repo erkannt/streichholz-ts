7233fc615af63d1ae15b573c2989831967f53b99
Daniel Haarhoff <daniel@rknt.de>
2023-02-15 09:39:21 +0000

    Make match test pass by copying from ts-adt

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,32 @@ export const cond: Cond = (cases, defaultBehaviour) => (input) => {
   return defaultBehaviour(input);
 };
 
-type Match = <A>(cases: Record<string, unknown>) => (input: A) => A;
+type MakeADTMember<D extends string, ADT, Type extends string> = Extract<
+  ADT,
+  Record<D, Type>
+>;
 
-export const match: Match = () => (input) => {
-  return input;
+type MakeMatchObj<D extends string, ADT extends Record<D, string>, Z> = {
+  [K in ADT[D]]: (v: MakeADTMember<D, ADT, K>) => Z;
+};
+
+type MakeReturns<
+  D extends string,
+  ADT extends Record<D, string>,
+  M extends MakeMatchObj<D, ADT, unknown>
+> = {
+  [K in keyof M]: ReturnType<M[K]>;
+}[keyof M];
+
+type Match = <
+  D extends string,
+  A extends Record<D, string>,
+  M extends MakeMatchObj<D, A, unknown>
+>(
+  matchObj: M,
+  discriminant: D
+) => (input: A) => MakeReturns<D, A, M>;
+
+export const match: Match = (cases, discriminant) => (input) => {
+  return cases[input[discriminant]](input as any) as any;
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -46,18 +46,18 @@ describe("cond", () => {
 });
 
 describe("match", () => {
-  it.failing(
-    "passes input to case whose key matches the value of the tag field",
-    () => {
-      const behaviour = jest.fn(() => undefined);
-      const input = { _tag: "foo", value: 42 };
-      pipe(
-        input,
-        match({
+  it("passes input to case whose key matches the value of the tag field", () => {
+    const behaviour = jest.fn(() => undefined);
+    const input = { _tag: "foo", value: 42 };
+    pipe(
+      input,
+      match(
+        {
           foo: behaviour,
-        })
-      );
-      expect(behaviour).toHaveBeenCalledWith(input);
-    }
-  );
+        },
+        "_tag"
+      )
+    );
+    expect(behaviour).toHaveBeenCalledWith(input);
+  });
 });
