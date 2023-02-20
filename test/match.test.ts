8532dc3a78c228d0cbcdae7384a136d4ffe5163b
Daniel Haarhoff <daniel@rknt.de>
2023-02-20 16:26:27 +0000

    Rudimentary test of matchOn

--- test/match.test.ts
+++ test/match.test.ts
@@ -1,13 +1,27 @@
 import { pipe } from "fp-ts/function";
-import { match } from "../src";
+import { match, matchOn } from "../src";
 
 describe("match", () => {
-  it("passes input to case whose key matches the value of the tag field", () => {
+  it("passes input to case whose key matches the value of the '_tag' field", () => {
     const behaviour = jest.fn(() => undefined);
     const input = { _tag: "foo", value: 42 };
     pipe(
       input,
       match({
+        foo: behaviour,
+      })
+    );
+    expect(behaviour).toHaveBeenCalledWith(input);
+  });
+});
+
+describe("matchOn", () => {
+  it("passes input to case whose key matches the value of the specified tag field", () => {
+    const behaviour = jest.fn(() => undefined);
+    const input = { name: "foo", value: 42 };
+    pipe(
+      input,
+      matchOn("name", {
         foo: behaviour,
       })
     );
