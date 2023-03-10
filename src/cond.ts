797c0fcbf475af44566f2f0e0a12b30c0bff7f8d
Daniel Haarhoff <daniel@rknt.de>
2023-03-10 08:03:09 +0000

    Sketch out rough shape of cond

--- src/cond.ts
+++ src/cond.ts
@@ -1,4 +1,5 @@
 import { Predicate } from "fp-ts/lib/Predicate";
+import { Refinement } from 'fp-ts/lib/Refinement';
 
 type CondP = <A, B>(
   cases: ReadonlyArray<[Predicate<A>, (a: A) => B]>,
@@ -12,4 +13,18 @@ export const condP: CondP = (cases, defaultBehaviour) => (input) => {
     }
   }
   return defaultBehaviour(input);
+};
+
+type Cond = <A, B extends A, C>(
+  cases: ReadonlyArray<[Refinement<A, B>, (b: B) => C]>,
+) => (input: A) => C;
+
+export const cond: Cond = (cases) => (input) => {
+  for (const [predicate, behaviour] of cases) {
+    if (predicate(input)) {
+      return behaviour(input);
+    }
+  }
+  throw new Error("This should be prevented from happening at compile time");
+  
 };
