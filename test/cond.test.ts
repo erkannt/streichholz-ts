767ead9d753f3da97916c5fc5d1ad74ea973a936
Daniel Haarhoff <daniel@rknt.de>
2023-03-10 08:09:24 +0000

    Write test for cond that doesn't compile to pull better signature into existence

--- test/cond.test.ts
+++ test/cond.test.ts
@@ -1,5 +1,7 @@
+import { number } from 'fp-ts';
 import { pipe } from "fp-ts/function";
 import { condP } from "../src";
+import { cond } from '../src/cond';
 
 describe("condP", () => {
   it("passes input to first condition that returns true", () => {
@@ -44,3 +46,27 @@ describe("condP", () => {
     expect(defaultBehaviour).toHaveBeenCalledWith(input);
   });
 });
+
+describe('cond', () => {
+  it("passes input to first condition that returns true", () => {
+    const behaviour1 = jest.fn(() => undefined);
+    const behaviour2 = jest.fn(() => undefined);
+    type Input = number | string
+    const input = 5;
+
+    pipe(
+      input as Input,
+      cond(
+        [
+          [(i): i is number => typeof i === 'number', behaviour1],
+          // @ts-expect-error
+          [(i): i is string => typeof i === 'string', behaviour2],
+        ],
+      )
+    );
+    expect(behaviour1).not.toHaveBeenCalled;
+    expect(behaviour2).toHaveBeenCalledWith(input);
+  });
+  
+  it.todo('enforces that the provided refinements cover all cases of a union passed as input')
+})
