3a14dcc90369cb396b6a7c9715dc2773e7f9cfcb
Daniel Haarhoff <daniel@rknt.de>
2024-11-08 10:04:35 +0000

    Derive tagged constructors

--- src/construct-union.ts
+++ src/construct-union.ts
@@ -1,22 +1,35 @@
 import { pipe } from "fp-ts/lib/function";
+import * as R from "fp-ts/Record";
 
-type Union<
+type MakeTaggedUnion<
   T extends Record<string, (...args: any[]) => Record<string, unknown>>
 > = {
   [K in keyof T]: { _tag: K } & ReturnType<T[K]>;
 }[keyof T];
 
+type TaggedConstructors<
+  T extends Record<string, (...args: any[]) => Record<string, unknown>>
+> = {
+  [K in keyof T]: (...args: Parameters<T[K]>) => ReturnType<T[K]> & { _tag: K };
+};
+
 type Member<T extends { _tag: string }, K extends T["_tag"]> = T & { _tag: K };
 
 type Values<T extends { _tag: string }> = Omit<T, "_tag">;
 
-const FooOrBar = {
+declare function makeTaggedConstructors<
+  A extends Record<string, (...args: any[]) => Record<string, unknown>>
+>(taglessConstructors: A): TaggedConstructors<A>;
+
+const constructors = {
   Foo: () => ({}),
   Bar: (value: number) => ({ value }),
 };
 
-type FooOrBar = Union<typeof FooOrBar>;
-type Bar = Member<FooOrBar, 'Bar'>;
+const FooOrBar = makeTaggedConstructors(constructors);
+type FooOrBar = MakeTaggedUnion<typeof constructors>;
+
+type Bar = Member<FooOrBar, "Bar">;
 type BarValues = Values<Bar>;
 
-const bar = pipe(42, FooOrBar.Bar);
+const bar: Bar = pipe(42, FooOrBar.Bar);
