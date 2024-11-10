83b513dfb6692a62e37ba489696f069d6cfd05a4
Daniel Haarhoff <daniel@rknt.de>
2024-11-10 17:51:58 +0000

    Write a test list for the tagged union code

---
+++ test/construct-union.test.ts
@@ -0,0 +1,21 @@
+describe('construct-union', () => {
+	it.todo('plays nice with match')
+	describe('makeTaggedConstructors', () => {
+		it.todo('returns the correct type')
+		it.todo('produces constructors that add the right tag')
+	})
+	describe('MakeTaggedUnion', () => {
+		it.todo('returns the correct type')
+	})
+	describe('Member', () => {
+		it.todo('returns the type of the selected member including the tag')
+		it.todo('prevents access to members that are not part of the tagged union')
+	})
+	describe('Values', () => {
+		it.todo('returns the type of the selected member\'s values excluding the tag')
+		it.todo('prevents access to members that are not part of the tagged union')
+	})
+	describe('MakeTaggedUnion', () => {
+		it.todo('returns the correct type')
+	})
+})
